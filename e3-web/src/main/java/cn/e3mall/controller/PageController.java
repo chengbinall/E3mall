3ae897ba803c02a3e3c0d186a96fcc3312ba2218
chengbin <m18040314039_1@163.com>
2018-01-25 20:20:47 +0800

    这是主干的代码

--- e3-web/src/main/java/cn/e3mall/controller/PageController.java
+++ e3-web/src/main/java/cn/e3mall/controller/PageController.java
@@ -3,10 +3,9 @@ package cn.e3mall.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class PageController {
-
+//这是主干的代码
 	@RequestMapping("/")
 	public String index(){
 		//直接跳转到后台的首页哦.
