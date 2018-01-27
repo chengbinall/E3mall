651b7b00bf932956a8aad87ee3ef3364a6482dfa
chengbin <m18040314039_1@163.com>
2018-01-27 17:58:27 +0800

    商品添加

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/serviceImpl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/serviceImpl/ItemServiceImpl.java
@@ -1,24 +1,29 @@
 package cn.e3mall.serviceImpl;
 
+import java.util.Date;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.Constant;
 import cn.e3mall.pojo.DataGridResult;
+import cn.e3mall.pojo.E3Result;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
+import cn.e3mall.utils.IDUtils;
 @Service
 public class ItemServiceImpl implements ItemService {
      //注入mapper
 	@Autowired
 	public  TbItemMapper  tbItemMapper;
-
+	@Autowired
+	public  TbItemDescMapper  tbItemDescMapper;
 	@Override
 	public TbItem findItmById(long itmId) {
 		TbItem tbItem = tbItemMapper.selectByPrimaryKey(itmId);
@@ -40,5 +45,31 @@ public class ItemServiceImpl implements ItemService {
 		result.setRows(list);
 		result.setTotal(total.intValue());
 		return result;
+	}
+	@Override
+	public Integer save(String desc, TbItem tbItem){
+		//生成商品的id，同过工具类生成
+		long itemId = IDUtils.genItemId();
+		tbItem.setId(itemId);
+		//手动添加商品里面没有缺少的参数值
+		tbItem.setCreated(new Date());
+		tbItem.setUpdated(new Date());
+		tbItem.setStatus(Constant.TBITEM_STATUS_1);
+		//添加商品到数据库,需要注意的是下面的插入是如果该值为null就不插入数据库的哦
+		int insert = tbItemMapper.insertSelective(tbItem);
+		//创建商品描述表需要的pojo
+		TbItemDesc tbItemDesc =new TbItemDesc();
+		tbItemDesc.setCreated(new Date());
+		tbItemDesc.setItemDesc(desc);
+		tbItemDesc.setItemId(itemId);
+		tbItemDesc.setUpdated(new Date());
+		//添加商品描述到数据库
+		int insertSelective = tbItemDescMapper.insertSelective(tbItemDesc);
+		if(insertSelective!=1||insert!=1){
+			return 1;
+		}
+		return 0;
+		
+		
 	}
 }
