c2214ccf3bdd0f8a38ebea06ddd0948c0bfaa61a
chengbin <m18040314039_1@163.com>
2018-01-27 18:00:44 +0800

    商品添加

---
+++ e3-common/src/main/java/cn/e3mall/pojo/Constant.java
@@ -0,0 +1,11 @@
+package cn.e3mall.pojo;
+
+public class Constant {
+//	商品状态，1-正常，2-下架，3-删除
+public static final Byte TBITEM_STATUS_1=1;
+public static final Byte TBITEM_STATUS_2=2;
+public static  final Byte TBITEM_STATUS_3=3;
+//添加商品是否成功 1表示成功 0 表示失败了
+public static  final Integer SAVE_TBITEM_STATUS_1=1;
+public static  final Integer SAVE_TBITEM_STATUS_0=0;
+}
