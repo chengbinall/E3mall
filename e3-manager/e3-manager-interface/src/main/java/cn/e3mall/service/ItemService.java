144a62fba627fb8ffa509f3dbf9da14c140e3ca0
chengbin <m18040314039_1@163.com>
2018-01-27 17:57:52 +0800

    商品添加功能的实现

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,6 +1,7 @@
 package cn.e3mall.service;
 
 import cn.e3mall.pojo.DataGridResult;
+import cn.e3mall.pojo.E3Result;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
@@ -8,5 +9,7 @@ public interface ItemService {
 public	TbItem findItmById(long itmId);
 
 public DataGridResult findItmList(Integer page, Integer rows);
+
+public Integer  save(String desc, TbItem tbItem) ;
 
 }
