8c18aaad51b084be604698937e478c321c87ec07
chengbin <m18040314039_1@163.com>
2018-01-30 16:34:45 +0800

    common

--- e3-common/src/main/java/cn/e3mall/pojo/Constant.java
+++ e3-common/src/main/java/cn/e3mall/pojo/Constant.java
@@ -1,11 +1,17 @@
 package cn.e3mall.pojo;
 
 public class Constant {
-//	商品状态，1-正常，2-下架，3-删除
+//	数据库该字段的状态，1-正常，2-下架，3-删除
 public static final Byte TBITEM_STATUS_1=1;
 public static final Byte TBITEM_STATUS_2=2;
 public static  final Byte TBITEM_STATUS_3=3;
-//添加商品是否成功 1表示成功 0 表示失败了
+//1-正常，2-下架，3-删除
+public static final Integer E3MALL_STATUS_1=1;
+public static final Integer E3MALL_STATUS_2=2;
+public static  final Integer E3MALL_STATUS_3=3;
+//表示保存操作成功 1表示成功 0 表示失败了
 public static  final Integer SAVE_TBITEM_STATUS_1=1;
 public static  final Integer SAVE_TBITEM_STATUS_0=0;
+//响应的状态值0  表示失败了
+public static  final Integer RESULT_STATUS_0=0;
 }
