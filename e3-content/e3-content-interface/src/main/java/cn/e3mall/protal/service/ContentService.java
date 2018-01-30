3d6481cb38d752a89fd4df54831ef6b962c49f96
chengbin <m18040314039_1@163.com>
2018-01-30 22:59:55 +0800

    完成了后台管理系统的所有的功能，除了solr

--- e3-content/e3-content-interface/src/main/java/cn/e3mall/protal/service/ContentService.java
+++ e3-content/e3-content-interface/src/main/java/cn/e3mall/protal/service/ContentService.java
@@ -1,9 +1,17 @@
 package cn.e3mall.protal.service;
 
 import cn.e3mall.pojo.DataGridResult;
+import cn.e3mall.pojo.E3Result;
+import cn.e3mall.pojo.TbContent;
 
 public interface ContentService {
 
 	DataGridResult findContentbyCategoryId(Integer page, Integer rows, Long categoryId);
+
+	E3Result saveContent(TbContent tbContent);
+
+	E3Result updateContent(TbContent tbContent);
+
+	E3Result deleteContent(String ids);
 
 }
